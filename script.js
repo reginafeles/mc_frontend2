047903f8e23b385ccc8757122b21e0c63bc0aedd
Eliseykina Kate <90447294+reginafeles@users.noreply.github.com>
2024-08-14 00:18:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,141 +6,25 @@ let height = Math.max(parseInt(getComputedStyle(team1).getPropertyValue('height'
 team1.style.height = `${height}px`
 team2.style.height = `${height}px`
 team3.style.height = `${height}px`
-
 function scrollDown() {
     window.scrollTo({top: window.innerHeight, behavior: 'smooth' });
 }
-
 let button = document.querySelector('.arrow-next')
 button.addEventListener('click', scrollDown)
 
-// Код, который нужно исправить
-let item_name_objects = document.querySelectorAll('.merch-item-name')
-let item_names = ['Тарелка', 'Кружка', 'Ракета', 'Марсоход']
-for (let i = 0; i < item_name_objects.length; i += 1) {
-   item_name_objects[i].innerHTML = item_names[i]
-}
+// Тут что-то интересное:
+let change_theme_btn = document.querySelector('.switch-theme-button')
+let header = document.querySelector('header')
+let header_background = 'url(/uploads/2022/11/bg_mars.png)'
 
-let item_descriptions = ['Тарелка с принтом Марса. Доступна в красном и белом цветах', 'Кружка с принтом Марса. Доступа в красном и белом цветах', 'Масштабная модель одной из ракет Galaxy', 'Масштабная модель масохода (мы покупаем их у NASA)']
-let item_prices = [1000, 1000, 2500, 5000]
-let item_description_objects = document.querySelectorAll('.merch-item-descr')
-let item_price_objects = document.querySelectorAll('.merch-item-price')
-for (let i = 0; i < item_description_objects.length; i += 1) {
-    item_description_objects[i].innerHTML = item_descriptions[i]
-    item_price_objects[i].innerHTML = item_prices[i]
-}
- 
-
-let total_price = 0
-let cart_items_count = document.querySelector('.cart-amount')
-let add_to_cart_buttons = document.querySelectorAll('.add-to-cart-btn')
-
-let cart_button = document.querySelectorAll('.nav-item')[4]
-let cart_button_text = cart_button.querySelector('.cart-text')
-
-for (let i = 0; i < add_to_cart_buttons.length; i += 1) {
-    add_to_cart_buttons[i].addEventListener('click', function() {
-        cart_items_count.innerHTML = +cart_items_count.innerHTML + 1
-        total_price += item_prices[i]
-        if (cart_button_text.innerHTML != 'Корзина') {
-            cart_button_text.innerHTML = total_price
-        }
-    })
-}
-cart_button.addEventListener('click', function() {
-    if (cart_button_text.innerHTML == 'Корзина') {
-        cart_button_text.innerHTML = total_price
+change_theme_btn.addEventListener('click', function() {
+    if (header_background == 'url(https://hi-news.ru/wp-content/uploads/2016/07/martian.jpg)') {
+        header.style.background = 'url(https://minio.nplus1.ru/app-images/226544/63018ed0a7d63_cover.jpg)'
+        header.style.backgroundSize = 'cover'
+        header_background = 'url(https://minio.nplus1.ru/app-images/226544/63018ed0a7d63_cover.jpg)'
     } else {
-        cart_button_text.innerHTML = 'Корзина'
+        header.style.background = 'url(https://hi-news.ru/wp-content/uploads/2016/07/martian.jpg)'
+        header.style.backgroundSize = 'cover'
+        header_background = 'url(https://hi-news.ru/wp-content/uploads/2016/07/martian.jpg)'
     }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Код, который меняет тему
-function change_theme() {
-   let header = document.querySelector('header')
-   if (current_background == 'url(/uploads/2022/11/bg_space.png)') {
-       current_background = 'url(/uploads/2022/11/bg_mars.png)'
-   } else {
-       current_background = 'url(/uploads/2022/11/bg_space.png)'
-   }
-   header.style.background = current_background
-   header.style.backgroundSize = 'cover'
-}
- 
-let change_theme_button = document.querySelector('.switch-theme-button')
-let current_background = 'url(/uploads/2022/11/bg_space.png)'
-change_theme_button.addEventListener('click', change_theme)
- 
-
-// Подсчет стоимости билета
-function calc_price() {
-   let days_input = document.querySelector('.days-input')
-   let days_amount = days_input.value
-   if (days_amount == '') {
-       days_input.style.border = '1px solid #AD4851'
-   } else {
-       let one_day_price = +document.querySelector('.submit-select').value
-       let total = +days_amount * one_day_price
-       days_input.style.border = 'none'
-       days_input.style.borderBottom = '1px solid #5D4229'
-       alert(total)
-   }
-}
-let calc_button = document.querySelector('.submit-btn-calc')
-calc_button.addEventListener('click', calc_price)
-
-
-// Красивая кнопка 'Отправить'
-function make_transparent() {
-    send_btn.style.backgroundColor = 'transparent'
-}
- 
-function make_colorful() {
-    send_btn.style.backgroundColor = '#C2AB99'
-}
-
-let send_btn = document.querySelector('.submit-btn-send')
-
-send_btn.addEventListener('mouseenter', make_transparent)
-send_btn.addEventListener('mouseleave', make_colorful)
-
-// Пасхалка
-function easter_egg() {
-    let mars1 = document.querySelector('.mars1')
-    let mars2 = document.querySelector('.mars2')
-    let mars3 = document.querySelector('.mars3')
-    let mars4 = document.querySelector('.mars4')
-    let mars5 = document.querySelector('.mars5')
-    mars1.style.color = '#AD4851'
-    mars2.style.color = '#AD4851'
-    mars3.style.color = '#AD4851'
-    mars4.style.color = '#AD4851'
-    mars5.style.color = '#AD4851'
-}
- 
-let mars = document.querySelector('.mars1')
-mars.addEventListener('click', easter_egg)
